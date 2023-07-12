9bc3fe591341f53051142ebf0870f13b89bb42b3
Pedro Henrique Fogaça Santos <pedrofogacami@gmail.com>
2023-07-12 17:36:30 +0000

    feat: login and register

--- setup/settings.py
+++ setup/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = str(os.getenv('DJANGO_SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-hdind-usuariosteste-4wff7mv7k8t.ws-us101.gitpod.io']
 
 
 # Application definition
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, logout as logout_django, login as login_django
+from django.contrib.auth.decorators import login_required
+
+
+@login_required(login_url='/login/')
+def snakegame(request):
+    return render(request, 'snakegame.html')
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
+        user = authenticate(username=username, password=password)
+
+        if user:
+            login_django(request, user)
+            return snakegame(request)
+        else:
+            return HttpResponse('Email ou senha inválidos')
 
 def register(request):
     if request.method == 'GET':
@@ -23,4 +41,5 @@ def register(request):
             return HttpResponse('Usuário cadastrado com sucesso.')
 
 def logout(request):
+    logout_django(request)
     return HttpResponse('Usuário saiu com sucesso.')
