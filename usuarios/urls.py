48c8efde4a125e3108989416a3874ba399e6e6b6
Pedro Henrique Fogaça Santos <p.fogaca@dasa.com.br>
2023-07-13 11:59:35 -0300

    feat: Score and Replace Fruit

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
     path('logout/', views.logout, name='logout'),
-    path('snakegame', views.snakegame, name='snakegame'),
-    path('testes', views.testes)
+    path('login/snakegame', views.snakegame, name='snakegame')
 ]
