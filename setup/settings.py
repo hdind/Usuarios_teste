e45e0d08ef7425817d9fb0418beff2a56a0b49af
Pedro Henrique Fogaça Santos <pedrofogacami@gmail.com>
2023-07-14 01:07:36 +0000

    chore: scoreboard and logout

--- setup/settings.py
+++ setup/settings.py
@@ -18,10 +18,21 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-hdind-usuariosteste-4wff7mv7k8t.ws-us101.gitpod.io',
+    '8000-hdind-usuariosteste-6xvisu9amg4.ws-us101.gitpod.io',
     '127.0.0.1:8000', 
     '127.0.0.1'
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    '8000-hdind-usuariosteste-4wff7mv7k8t.ws-us101.gitpod.io',
+    '8000-hdind-usuariosteste-6xvisu9amg4.ws-us101.gitpod.io',
+    '127.0.0.1:8000', 
+    '127.0.0.1'
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://8000-hdind-usuariosteste-6xvisu9amg4.ws-us101.gitpod.io'
+]
 
 # Application definition
 
