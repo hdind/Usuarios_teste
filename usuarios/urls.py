3a7e84bec837092513afdc0230cf26facb06a68e
Pedro Henrique Fogaça Santos <p.fogaca@dasa.com.br>
2023-07-26 09:42:41 -0300

    first commit

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
     path('logout/', views.logout, name='logout'),
-    path('game', views.game, name='game')
+    path('game/', views.game, name='game')
 ]
